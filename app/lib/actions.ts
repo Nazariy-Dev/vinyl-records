5f9250bdc6e21081aedb55d1f21e67bad871e35f
Nazariy-Dev <139453365+Nazariy-Dev@users.noreply.github.com>
2024-12-03 09:57:13 +0200

    Auth bug fixed

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -161,15 +161,15 @@ export async function authenticate(
 
         await signIn('credentials', formData);
     } catch (error: any) {
-        // if (error instanceof AuthError) {
+        if (error instanceof AuthError) {
             switch (error.type) {
                 case 'CredentialsSignin':
                     return 'Invalid credentials.';
                 default:
                     return 'Something went wrong.';
             }
-        // }
-        // throw error;
+        }
+        throw error;
     }
 }
 
